33fa2b171f7d0a0f15346af78948e55a567baf7b
Inuniku <inuniku262@gmail.com>
2022-10-30 14:43:01 +0100

    Adjusted Waterforce

--- Assets/com.fsb.captainclawhand/Scripts/Player/WaterForce.cs
+++ Assets/com.fsb.captainclawhand/Scripts/Player/WaterForce.cs
@@ -33,7 +33,7 @@ public class WaterForce : MonoBehaviour
         {
             float multiplier = Mathf.Pow(depth, 2f);
             _rigidbody.AddForce(_upForce * Time.fixedDeltaTime * _upForceStrength * multiplier, ForceMode.Force);
-            
+
             //Velocity-based drag (reduces y speed when under-water)
             float downSpeed = _rigidbody.velocity.y;
             _rigidbody.AddForce(new Vector3(0, -downSpeed * _downspeedAntiForce * Time.fixedDeltaTime, 0), ForceMode.VelocityChange);
@@ -41,7 +41,7 @@ public class WaterForce : MonoBehaviour
 
 
         var up = _rigidbody.transform.up;
-        if (Mathf.Abs(up.x) > 0.1f || Mathf.Abs(up.z) > 0.1f)
+        /*if (Mathf.Abs(up.x) > 0.1f || Mathf.Abs(up.z) > 0.1f)
         {
             var upPlane = new Vector3(up.x, 0f, up.z);
             var upPlaneNormalized = upPlane.normalized;
@@ -57,7 +57,38 @@ public class WaterForce : MonoBehaviour
             Debug.Log($"<color=\"green\">levelStrength: {levelStrength}</color>");
 
             _rigidbody.AddForceAtPosition(forceDirection * levelStrength * Time.fixedDeltaTime * _levelStrength, forcePosition);
-        }
+        }*/
+
+        // Rotate up Torque
+
+        Quaternion deltaQuat = Quaternion.FromToRotation(_rigidbody.transform.up, Vector3.up);
+
+        Vector3 axis;
+        float angle;
+        deltaQuat.ToAngleAxis(out angle, out axis);
+
+        float dampenFactor = 0.8f; // this value requires tuning
+        _rigidbody.AddTorque(-_rigidbody.angularVelocity * dampenFactor, ForceMode.Acceleration);
+
+        float adjustFactor = 0.5f; // this value requires tuning
+        _rigidbody.AddTorque(axis.normalized * angle * adjustFactor, ForceMode.Acceleration);
+
+
+
+        /*var upPlane = new Vector3(up.x, 0f, up.z);
+        var upPlaneNormalized = upPlane.normalized;
+
+        var forcePosition = _rigidbody.transform.position + upPlaneNormalized;
+
+        float levelStrength = Vector3.Dot(_levelUpReference, up);
+        levelStrength = 1f - ((levelStrength + 1) * 0.5f);
+
+        var upCenter = _rigidbody.transform.position + _levelUpReference;
+        var forceDirection = upCenter - forcePosition;
+
+        Debug.Log($"<color=\"green\">levelStrength: {levelStrength}</color>");
+
+        _rigidbody.AddForceAtPosition(forceDirection * levelStrength * Time.fixedDeltaTime * _levelStrength, forcePosition);*/
 
 
         //var upCenter = _rigidbody.transform.position + _levelUpReference;
@@ -71,5 +102,28 @@ public class WaterForce : MonoBehaviour
 
         //    var upOffCenterPlane = new Vector3(upOffCenter.x, 0f, upOffCenter.z);
         //}
+    }
+
+    public Vector3 ComputeTorque(Quaternion desiredRotation)
+    {
+        //q will rotate from our current rotation to desired rotation
+        Quaternion q = desiredRotation * Quaternion.Inverse(transform.rotation);
+        //convert to angle axis representation so we can do math with angular velocity
+        Vector3 x;
+        float xMag;
+        q.ToAngleAxis(out xMag, out x);
+        x.Normalize();
+        //w is the angular velocity we need to achieve
+        Vector3 w = x * xMag * Mathf.Deg2Rad / Time.fixedDeltaTime;
+        w -= _rigidbody.angularVelocity;
+        //to multiply with inertia tensor local then rotationTensor coords
+        Vector3 wl = transform.InverseTransformDirection(w);
+        Vector3 Tl;
+        Vector3 wll = wl;
+        wll = _rigidbody.inertiaTensorRotation * wll;
+        wll.Scale(_rigidbody.inertiaTensor);
+        Tl = Quaternion.Inverse(_rigidbody.inertiaTensorRotation) * wll;
+        Vector3 T = transform.TransformDirection(Tl);
+        return T;
     }
 }
